930a2cbe4cf3ce370312ca79251ba305d284f70c
YouLim Oh <youlimoh@YouLimui-MacBookAir.local>
2023-01-25 01:01:31 +0900

    up

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-def main(request) :
-    pass
+def main_page(request) :
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <form action="/your_metro/" method="post">
+            <p><textarea name="text" placeholder="TEXT"></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+    </body>
+    </html>
+    ''')
+
+@csrf_exempt
+def your_metro(request) :
+    if request.method == "POST" :
+        text = request.POST['text']
+        return HttpResponse(f'''
+        {text}
+        ''')
